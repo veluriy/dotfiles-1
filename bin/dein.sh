ff368dda68d8d885ec63139049c803c359fee18c
wurzeit <hiccap.master.jedi@gmail.com>
2022-02-23 20:05:07 +0900

    [move] dein.toml dein_lazy.toml

--- bin/dein.sh
+++ bin/dein.sh
@@ -7,5 +7,5 @@ sh ./installer.sh ~/.cache/dein
 rm ./installer.sh
 mkdir -pv ~/.config/nvim/
 
-ln -snfv "$DOTPATH/.vim/dein.toml" "$HOME/.config/nvim/dein.toml"
-ln -snfv "$DOTPATH/.vim/dein_lazy.toml" "$HOME/.config/nvim/dein_lazy.toml"
+ln -snfv "$DOTPATH/.vim/rc/dein.toml" "$HOME/.config/nvim/dein.toml"
+ln -snfv "$DOTPATH/.vim/rc/dein_lazy.toml" "$HOME/.config/nvim/dein_lazy.toml"
