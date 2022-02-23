54acbaef575cec748249219566a67578792a0682
wurzeit <hiccap.master.jedi@gmail.com>
2022-02-23 21:02:25 +0900

    [add] coc setting file

--- bin/vim/coc.sh
+++ bin/vim/coc.sh
@@ -4,3 +4,5 @@ DOTPATH=~/dotfiles
 
 mkdir -pv $HOME/.config/nvim/plugins
 ln -snfv $DOTPATH/.vim/rc/coc.rc.vim $HOME/.config/nvim/plugins/coc.rc.vim
+ln -snfv $DOTPATH/.vim/rc/coc-setting.json $HOME/.vim/coc-setting.json
+ln -snfv $DOTPATH/.vim/rc/coc-setting.json $HOME/.nvim/coc-setting.json
