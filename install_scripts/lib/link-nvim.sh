d8a59a63f6e0da09318c9da27770b59daa800c67
wurzeit <hiccap.master.jedi@gmail.com>
2022-02-28 20:54:24 +0900

    [create] link nvim sh

---
+++ install_scripts/lib/link-nvim.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -ue
+
+DOTPATH=~/dotfiles
+INIT_PATH=$DOTPATH/.config/vim/.vimrc
+NVIM_CONFIG_PATH=$HOME/.config/nvim
+
+mkdir -pv NVIM_CONFIG_PATH
+ln -snfv $INIT_PATH $NVIM_CONFIG_PATH/init.vim
