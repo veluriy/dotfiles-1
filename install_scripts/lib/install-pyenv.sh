6425c2351aa68dab060a8b0b6f53b25133ac43e8
wurzeit <hiccap.master.jedi@gmail.com>
2022-03-03 14:58:30 +0900

    [add] pyenv-virtualenv

--- install_scripts/lib/install-pyenv.sh
+++ install_scripts/lib/install-pyenv.sh
@@ -4,6 +4,7 @@ set -ue
 
 install_pyenv() {
   git clone https://github.com/yyuu/pyenv.git ~/.pyenv
+  git clone https://github.com/yyuu/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
 }
 
 install_pyenv
