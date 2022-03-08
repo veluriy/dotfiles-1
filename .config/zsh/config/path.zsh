535cc6df7343d2988043bf5663db6a830f1fc61a
wurzeit <hiccap.master.jedi@gmail.com>
2022-03-08 23:22:01 +0900

    [add] aa

--- .config/zsh/config/path.zsh
+++ .config/zsh/config/path.zsh
@@ -1,3 +1,10 @@
+#              _   _     
+#  _ __   __ _| |_| |__  
+# | '_ \ / _` | __| '_ \ 
+# | |_) | (_| | |_| | | |
+# | .__/ \__,_|\__|_| |_|
+# |_|
+
 PATH="$HOME/.local/bin:$PATH"
 PATH="$HOME/.cargo/bin:$PATH"
 export PYENV_ROOT="$HOME/.pyenv"
