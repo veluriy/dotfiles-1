dd35fc0301fe8d73ca624635594a51c57b21abd9
wurzeit <hiccap.master.jedi@gmail.com>
2022-03-09 00:03:33 +0900

    [add]

---
+++ install_scripts/lib/install-git-completion-prompt.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -ue
+
+install_git_completion_prompt() {
+  mkdir -p ~/.zsh/completion
+  curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.zsh -o ~/.zsh/completion/_git
+  curl https://raw/githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -o ~/.zsh/completion/git-prompt.sh
+
+  chmod a+x ~/.zsh/completion/_git
+  chmod a+x ~/.zsh/completion/git-prompt.zsh
+
+  rm -f ~/.zcompdump
+}
