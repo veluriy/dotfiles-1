33256c489e5ed6fa221bb162e329235c0aa0db47
wurzeit <hiccap.master.jedi@gmail.com>
2022-03-13 21:53:11 +0900

    [fix]

--- install_scripts/lib/install-nvim.sh
+++ install_scripts/lib/install-nvim.sh
@@ -3,11 +3,13 @@
 set -ue
 
 install_neovim() {
-  sudo apt update
-  sudo apt install neovim
-  sudo pip3 install pynvim
-  sudo pip3 install neovim-remote
-  sudo pip3 install pylint
+  mkdir -pv ~/repo
+  cd ~/repo
+  git clone https://github.com/neovim/neovim.git
+  cd neovim
+  make CMAKE_BUILD_TYPE=Release
+  sudo make install
+  cd ~
 }
 
 install_neovim
