de8a1d67586af0f271eadcb3c626f7dd048cd83e
wurzeit <hiccap.master.jedi@gmail.com>
2022-03-21 22:54:55 +0900

    [add] functions link

--- install_scripts/lib/link-fish.sh
+++ install_scripts/lib/link-fish.sh
@@ -8,3 +8,4 @@ FISHPATH=$HOME/.config/fish
 mkdir -pv $FISHPATH
 ln -snfv $FISHCONFIGPATH/config.fish $FISHPATH/fish.config
 ln -snfv $FISHCONFIGPATH/conf.d $FISHPATH/conf.d
+ln -snfv $FISHCONFIGPATH/functions $FISHPATH/functions
