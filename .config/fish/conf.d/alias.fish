00a4e5138fd1918cd96121d650c9abe3e007e94d
wurzeit <hiccap.master.jedi@gmail.com>
2022-03-21 20:48:59 +0900

    [change] alias

--- .config/fish/conf.d/alias.fish
+++ .config/fish/conf.d/alias.fish
@@ -5,67 +5,67 @@
 #  \__,_|_|_|\__,_|___/
 
 # ls
-alias ls='ls -lhpaG'
-alias l='ls'
-alias ll='clear && l'
-alias ka='k -a'
-alias kk='clear && k -a'
+alias ls "ls -lhpaG"
+alias l "ls"
+alias ll "clear && l"
+alias ka "k -a"
+alias kk "clear && k -a"
 
 # cd
-alias d='cd ~/dotfiles'
-alias p='cd ~/projects'
-alias til='cd ~/projects/til'
-alias ..='cd ..'
-alias ...='cd ../..'
-alias ....='cd ../../..'
+alias d "cd ~/dotfiles"
+alias p "cd ~/projects"
+alias til "cd ~/projects/til"
+alias .. "cd .."
+alias ... "cd ../.."
+alias .... "cd ../../.."
 
 # rm
-alias rm='rm -iv'
+alias rm "rm -iv"
 
 # cp
-alias cp='cp -iv'
+alias cp "cp -iv"
 
 # apt
-alias agu='sudo apt update'
-alias agi='sudo apt install'
-alias agr='sudo apt remove'
+alias agu "sudo apt update"
+alias agi "sudo apt install"
+alias agr "sudo apt remove"
 
 # vim
-alias vim='nvim'
+alias vim "nvim"
 
 # tar
-alias tarx='tar -zxvf'
+alias tarx "tar -zxvf"
 
 # figlet
-alias aa='figlet'
+alias aa "figlet"
 
 # clear
-alias c='clear'
+alias c "clear"
 
 # stack
-alias ghci='stack ghci'
-alias ghc='stack ghc --'
-alias runghc='stack runghc --'
+alias ghci "stack ghci"
+alias ghc "stack ghc --"
+alias runghc "stack runghc --"
 
 # git
-alias g='git'
+alias g "git"
 
-alias ga='git add'
-alias gaa='git add --all'
+alias ga "git add"
+alias gaa "git add --all"
 
-alias gs='git status'
+alias gs "git status"
 
-alias gc='git commit -vm'
-alias gac='git commit -a -vm'
+alias gc "git commit -vm"
+alias gac "git commit -a -vm"
 
-alias gb='git branch'
-alias gba='git branch -a'
-alias gbr='git branch -r'
-alias gco='git checkout'
-alias gcob='git checkout -b'
+alias gb "git branch"
+alias gba "git branch -a"
+alias gbr "git branch -r"
+alias gco "git checkout"
+alias gcob "git checkout -b"
 
-alias gf='git fetch'
-alias gp='git push'
-alias gl='git pull'
+alias gf "git fetch"
+alias gp "git push"
+alias gl "git pull"
 
-alias gd='git diff'
+alias gd "git diff"
