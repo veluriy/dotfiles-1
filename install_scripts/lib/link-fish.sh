fa6619e8b0d9c15bf1b7bfa8dabeeab88c327787
wurzeit <hiccap.master.jedi@gmail.com>
2022-03-21 17:45:10 +0900

    [create] linker fish

---
+++ install_scripts/lib/link-fish.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -ue
+
+FISHCONFIGPATH=~/dotfiles/.config/fish
+FISHPATH=$HOME/.config/fish
+
+mkdir -pv $FISHPATH
+ln -snfv $FISHCONFIGPATH/config.fish $FISHPATH/fish.config
+ln -snfv $FISHCONFIGPATH/config $FISHPATH/config
