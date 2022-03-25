50352ea08bc108230a8c6ffa6b5ad410fcd139fa
wurzeit <hiccap.master.jedi@gmail.com>
2022-03-25 15:05:26 +0900

    [add] define for, [fix] define rep

--- .config/fish/functions/tcpp.fish
+++ .config/fish/functions/tcpp.fish
@@ -3,7 +3,8 @@ function tcpp
   echo \
   "#include <bits/stdc++.h>
 using namespace std;
-#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define for(i, a, b) for (int i=(int)(a); i < (int)(b); i++)
+#define rep(i, n) for(i, 0, n)
 
 int main() {
   cout << \"Hello, world\" << endl;
