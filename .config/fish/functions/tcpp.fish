7554e5b1ba0bb4e0c41cc4c237d5cc6f7ff36e7f
wurzeit <hiccap.master.jedi@gmail.com>
2022-03-25 15:11:33 +0900

    [add] macro

--- .config/fish/functions/tcpp.fish
+++ .config/fish/functions/tcpp.fish
@@ -6,7 +6,20 @@ using namespace std;
 #define for(i, a, b) for (int i=(int)(a); i < (int)(b); i++)
 #define rep(i, n) for(i, 0, n)
 
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+
+#define pb push_back
+
+#define print(V) for (auto v : (V)) cout << v << \" \"
+
+int N, M;
+vi Vec;
+
 int main() {
-  cout << \"Hello, world\" << endl;
+  cin >> N >> M;
+  Vec.pb(N);
+  Vec.pb(M);
+  print(Vec);
 }" > $argv
 end
