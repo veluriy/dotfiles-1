5d3177fe2b6dbfb14aa555744fac94e78e931ac8
wurzeit <hiccap.master.jedi@gmail.com>
2022-04-24 21:24:26 +0900

    [fix]

--- .config/fish/functions/tcpp.fish
+++ .config/fish/functions/tcpp.fish
@@ -5,9 +5,10 @@ function tcpp
 using namespace std;
 #define for(i, a, b) for (int i=(int)(a); i < (int)(b); i++)
 #define rep(i, n) for(i, 0, n)
-#define endl "\n"
+#define endl \"\n\"
 
 int main() {
   cout << \"Hello, world!\" << endl;
+  return 0;
 }" > $argv
 end
