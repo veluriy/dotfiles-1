104b9da9c52ed0b3c1e7d71d619e46262f8cecdd
wurzeit <hiccap.master.jedi@gmail.com>
2022-04-24 18:16:09 +0900

    [add] define

--- .config/fish/functions/tcpp.fish
+++ .config/fish/functions/tcpp.fish
@@ -5,6 +5,7 @@ function tcpp
 using namespace std;
 #define for(i, a, b) for (int i=(int)(a); i < (int)(b); i++)
 #define rep(i, n) for(i, 0, n)
+#define endl "\n"
 
 int main() {
   cout << \"Hello, world!\" << endl;
--- .config/fish/functions/tcppabc.fish
+++ .config/fish/functions/tcppabc.fish
@@ -4,6 +4,7 @@ function tcppabc
 using namespace std;
 #define for(i, a, b) for (int i=(int)(a); i < (int)(b); i++)
 #define rep(i, n) for(i, 0, n)
+#define endl "\n"
 
 int main() {
   cout << \"Hello, world!\" << endl;
