38f9b30031a6af7d9d9a811b46923e3950b9e1ec
wurzeit <hiccap.master.jedi@gmail.com>
2022-04-24 21:25:13 +0900

    [fix]

--- .config/fish/functions/tcppabc.fish
+++ .config/fish/functions/tcppabc.fish
@@ -1,17 +1,8 @@
 function tcppabc
-  echo \
-  "#include <bits/stdc++.h>
-using namespace std;
-#define for(i, a, b) for (int i=(int)(a); i < (int)(b); i++)
-#define rep(i, n) for(i, 0, n)
-#define endl "\n"
-
-int main() {
-  cout << \"Hello, world!\" << endl;
-}" > a.echop
-  cp -f a.cpp b.cpp
-  cp -f a.cpp c.cpp
-  cp -f a.cpp d.cpp
-  cp -f a.cpp e.cpp
-  cp -f a.cpp f.cpp
+  tcpp a.cpp
+  tcpp b.cpp
+  tcpp c.cpp
+  tcpp d.cpp
+  tcpp e.cpp
+  tcpp f.cpp
 end
