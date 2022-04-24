447ada9e1fd0cbc664b311cc48051167bda2c0b9
wurzeit <hiccap.master.jedi@gmail.com>
2022-04-24 16:48:40 +0900

    [delete] 不要なもの

--- .config/fish/functions/tcpp.fish
+++ .config/fish/functions/tcpp.fish
@@ -6,11 +6,6 @@ using namespace std;
 #define for(i, a, b) for (int i=(int)(a); i < (int)(b); i++)
 #define rep(i, n) for(i, 0, n)
 
-typedef vector<int> vi;
-typedef vector<vi> vvi;
-
-#define pb push_back
-
 int main() {
   cout << \"Hello, world!\" << endl;
 }" > $argv
