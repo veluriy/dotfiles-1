57d204cc46d750a5bbdf43cc53b1309569217665
wurzeit <hiccap.master.jedi@gmail.com>
2022-08-11 00:14:57 +0900

    add fzf, rust, c

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -2,4 +2,9 @@ vim.cmd('packadd vim-jetpack')
 require('jetpack').startup(function(use)
   use { 'tani/vim-jetpack', opt = 1 }-- bootstrap
   use { 'morhetz/gruvbox' }-- color scheme
+  use { 'junegunn/fzf', run = vim.fn['fzf#install'] }
+  use { 'junegunn/fzf.vim' }
+  use { 'rust-lang/rust.vim' }
+  use { 'justmao945/vim-clang' }
+  use { 'rhysd/vim-clang-format' }
 end)
