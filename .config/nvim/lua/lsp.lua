54f3d93b1f77cb9da53529e976496dc937f67eaa
wurzeit <hiccap.master.jedi@gmail.com>
2022-08-12 01:26:54 +0900

    add cool vim

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -17,18 +17,18 @@ end })
 
 -- 2. build-in LSP function
 -- keyboard shortcut
-vim.keymap.set('n', 'K',  '<cmd>lua vim.lsp.buf.hover()<CR>')
-vim.keymap.set('n', 'gf', '<cmd>lua vim.lsp.buf.formatting()<CR>')
-vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>')
-vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>')
-vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>')
-vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>')
-vim.keymap.set('n', 'gk', '<cmd>lua vim.lsp.buf.type_definition()<CR>')
-vim.keymap.set('n', 'gn', '<cmd>lua vim.lsp.buf.rename()<CR>')
-vim.keymap.set('n', 'ga', '<cmd>lua vim.lsp.buf.code_action()<CR>')
-vim.keymap.set('n', 'ge', '<cmd>lua vim.diagnostic.open_float()<CR>')
-vim.keymap.set('n', 'g]', '<cmd>lua vim.diagnostic.goto_next()<CR>')
-vim.keymap.set('n', 'g[', '<cmd>lua vim.diagnostic.goto_prev()<CR>')
+vim.keymap.set('n', 'bK',  '<cmd>lua vim.lsp.buf.hover()<CR>')
+vim.keymap.set('n', 'bgf', '<cmd>lua vim.lsp.buf.formatting()<CR>')
+vim.keymap.set('n', 'bgr', '<cmd>lua vim.lsp.buf.references()<CR>')
+vim.keymap.set('n', 'bgd', '<cmd>lua vim.lsp.buf.definition()<CR>')
+vim.keymap.set('n', 'bgD', '<cmd>lua vim.lsp.buf.declaration()<CR>')
+vim.keymap.set('n', 'bgi', '<cmd>lua vim.lsp.buf.implementation()<CR>')
+vim.keymap.set('n', 'bgk', '<cmd>lua vim.lsp.buf.type_definition()<CR>')
+vim.keymap.set('n', 'bgn', '<cmd>lua vim.lsp.buf.rename()<CR>')
+vim.keymap.set('n', 'bga', '<cmd>lua vim.lsp.buf.code_action()<CR>')
+vim.keymap.set('n', 'bge', '<cmd>lua vim.diagnostic.open_float()<CR>')
+vim.keymap.set('n', 'bg]', '<cmd>lua vim.diagnostic.goto_next()<CR>')
+vim.keymap.set('n', 'bg[', '<cmd>lua vim.diagnostic.goto_prev()<CR>')
 -- LSP handlers
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
   vim.lsp.diagnostic.on_publish_diagnostics, { virtual_text = false }
@@ -83,3 +83,37 @@ cmp.setup({
 --     { name = "cmdline" },
 --   },
 -- })
+
+local status, cmp = pcall(require, "cmp")
+if (not status) then return end
+local lspkind = require 'lspkind'
+
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.close(),
+    ['<CR>'] = cmp.mapping.confirm({
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true
+    }),
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'buffer' },
+  }),
+  formatting = {
+    format = lspkind.cmp_format({ with_text = false, maxwidth = 50 })
+  }
+})
+
+vim.cmd [[
+  set completeopt=menuone,noinsert,noselect
+  highlight! default link CmpItemKind CmpItemMenuDefault
+]]
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -9,7 +9,6 @@ require('jetpack').startup(function(use)
   use { 'rust-lang/rust.vim' }
   use { 'justmao945/vim-clang' }
   use { 'rhysd/vim-clang-format' }
-  use { 'airblade/vim-gitgutter' }
   use { 'neovim/nvim-lspconfig' }
   use { 'williamboman/mason.nvim' }
   use { 'williamboman/mason-lspconfig.nvim' }
@@ -20,15 +19,20 @@ require('jetpack').startup(function(use)
   use { 'hrsh7th/cmp-buffer' }
   use { 'hrsh7th/cmp-cmdline' }
   use { 'ibhagwan/fzf-lua' }
+  use { 'onsails/lspkind-nvim' }
+  use { 'L3MON4D3/LuaSnip' }
+  use { 'nvim-treesitter/nvim-treesitter' }
+  use { 'windwp/nvim-ts-autotag' }
+  use { 'windwp/nvim-autopairs' }
+  use { 'nvim-lua/plenary.nvim' }-- required telescope
+  use { 'BurntSushi/ripgrep' }-- required telescope
+  use { 'sharkdp/fd' }-- required telescope
+  use { 'nvim-telescope/telescope.nvim' }
+  use { 'nvim-telescope/telescope-file-browser.nvim' }
+  use { 'kyazdani42/nvim-web-devicons' }
+  use { 'akinsho/nvim-bufferline.lua' }--buffer line customize
+  use { 'glepnir/lspsaga.nvim' }
+  use { 'jose-elias-alvarez/null-ls.nvim' }
+  use { 'MunifTanjim/prettier.nvim' }
+  use { 'lewis6991/gitsigns.nvim' }-- git
 end)
-
--- 'airblade/gitgutter'
-vim.g.gitgutter_sign_added = '+'
-vim.g.gitgutter_sign_modified = '^'
-vim.g.gitgutter_sign_removed = '-'
-vim.highlight.create('GitGutterAdd', { guifg = '#009900', ctermfg = 2 }, false)
-vim.highlight.create('GitGutterChange', { guifg = '#bbbb00', ctermfg = 3 }, false)
-vim.highlight.create('GitGutterDelete', { guifg = '#ff2222', ctermfg = 1 }, false)
-vim.highlight.create('GitGutterAddLine', { ctermbg = 2 }, false)
-vim.highlight.create('GitGutterChangeLine', { ctermbg = 3 }, false)
-vim.highlight.create('GitGutterDeleteLine', { ctermbg = 1 }, false)
