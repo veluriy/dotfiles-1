3733ae6e574833ebb133a61002e1142985665cb0
wurzeit <hiccap.master.jedi@gmail.com>
2022-08-12 20:56:08 +0900

    change colorscheme gruvbox <- neosolorized

--- .config/nvim/lua/color.lua
+++ .config/nvim/lua/color.lua
@@ -1,43 +1,43 @@
 -- color.lua
 
---vim.cmd 'colorscheme gruvbox'
---vim.opt.background = 'dark'
---vim.highlight.create('Normal', { ctermbg = 'NONE', guibg = 'NONE' }, false)
---vim.highlight.create('NonText', { ctermbg = 'NONE', guibg = 'NONE' }, false)
---vim.highlight.create('SpecialKey', { ctermbg = 'NONE', guibg = 'NONE' }, false)
---vim.highlight.create('EndOfBuffer', { ctermbg = 'NONE', guibg = 'NONE' }, false)
---vim.highlight.create('LineNr', { ctermbg = 'NONE', guibg = 'NONE' }, false)
---vim.highlight.create('SignColumn', { ctermbg = 'NONE', guibg = 'NONE' }, false)
+vim.cmd 'colorscheme gruvbox'
+vim.opt.background = 'dark'
+vim.highlight.create('Normal', { ctermbg = 'NONE', guibg = 'NONE' }, false)
+vim.highlight.create('NonText', { ctermbg = 'NONE', guibg = 'NONE' }, false)
+vim.highlight.create('SpecialKey', { ctermbg = 'NONE', guibg = 'NONE' }, false)
+vim.highlight.create('EndOfBuffer', { ctermbg = 'NONE', guibg = 'NONE' }, false)
+vim.highlight.create('LineNr', { ctermbg = 'NONE', guibg = 'NONE' }, false)
+vim.highlight.create('SignColumn', { ctermbg = 'NONE', guibg = 'NONE' }, false)
 
-local status, n = pcall(require, 'neosolarized')
-if (not status) then return end
-
-n.setup({
-  comment_italics = true,
-})
-
-local cb = require('colorbuddy.init')
-local Color = cb.Color
-local colors = cb.colors
-local Group = cb.Group
-local groups = cb.groups
-local styles = cb.styles
-
-Color.new('black', '#000000')
-Group.new('CursorLine', colors.none, colors.base03, styles.NONE, colors.base1)
-Group.new('CursorLineNr', colors.yellow, colors.black, styles.NONE, colors.base1)
-Group.new('Visual', colors.none, colors.base03, styles.reverse)
-
-local cError = groups.Error.fg
-local cInfo = groups.Information.fg
-local cWarn = groups.Warning.fg
-local cHint = groups.Hint.fg
-
-Group.new('DiagnosticVirtualTextError', cError, cError:dark():dark():dark():dark(), styles.NONE)
-Group.new('DiagnosticVirtualTextInfo', cInfo, cInfo:dark():dark():dark(), styles.NONE)
-Group.new('DiagnosticVirtualTextWarn', cWarn, cWarn:dark():dark():dark(), styles.NONE)
-Group.new('DiagnosticVirtualTextHint', cHint, cHint:dark():dark():dark(), styles.NONE)
-Group.new('DiagnosticUnderlineError', colors.none, colors.none, styles.undercurl, cError)
-Group.new('DiagnosticUnderlineWarn', colors.none, colors.none, styles.undercurl, cWarn)
-Group.new('DiagnosticUnderlineInfo', colors.none, colors.none, styles.undercurl, cInfo)
-Group.new('DiagnosticUnderlineHint', colors.none, colors.none, styles.undercurl, cHint)
+--local status, n = pcall(require, 'neosolarized')
+--if (not status) then return end
+--
+--n.setup({
+--  comment_italics = true,
+--})
+--
+--local cb = require('colorbuddy.init')
+--local Color = cb.Color
+--local colors = cb.colors
+--local Group = cb.Group
+--local groups = cb.groups
+--local styles = cb.styles
+--
+--Color.new('black', '#000000')
+--Group.new('CursorLine', colors.none, colors.base03, styles.NONE, colors.base1)
+--Group.new('CursorLineNr', colors.yellow, colors.black, styles.NONE, colors.base1)
+--Group.new('Visual', colors.none, colors.base03, styles.reverse)
+--
+--local cError = groups.Error.fg
+--local cInfo = groups.Information.fg
+--local cWarn = groups.Warning.fg
+--local cHint = groups.Hint.fg
+--
+--Group.new('DiagnosticVirtualTextError', cError, cError:dark():dark():dark():dark(), styles.NONE)
+--Group.new('DiagnosticVirtualTextInfo', cInfo, cInfo:dark():dark():dark(), styles.NONE)
+--Group.new('DiagnosticVirtualTextWarn', cWarn, cWarn:dark():dark():dark(), styles.NONE)
+--Group.new('DiagnosticVirtualTextHint', cHint, cHint:dark():dark():dark(), styles.NONE)
+--Group.new('DiagnosticUnderlineError', colors.none, colors.none, styles.undercurl, cError)
+--Group.new('DiagnosticUnderlineWarn', colors.none, colors.none, styles.undercurl, cWarn)
+--Group.new('DiagnosticUnderlineInfo', colors.none, colors.none, styles.undercurl, cInfo)
+--Group.new('DiagnosticUnderlineHint', colors.none, colors.none, styles.undercurl, cHint)
