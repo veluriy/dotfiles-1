1d36f4701b79cb0346fcb11930d893196b86ff89
wurzeit <hiccap.master.jedi@gmail.com>
2022-08-19 23:16:44 +0900

    [add] nvim adp scala setting

--- .config/nvim/lua/metal.lua
+++ .config/nvim/lua/metal.lua
@@ -7,6 +7,33 @@ metals_config.setting = {
   excludePackages = { "akka.actor.typed.javadsl", "com.github.swagger.akka.javadsl" },
 }
 
+-- Debug settings if you're using nvim-dap
+local dap = require("dap")
+
+dap.configurations.scala = {
+  {
+    type = "scala",
+    request = "launch",
+    name = "RunOrTest",
+    metals = {
+      runType = "runOrTestFile",
+      --args = { "firstArg", "secondArg", "thirdArg" }, -- here just as an example
+    },
+  },
+  {
+    type = "scala",
+    request = "launch",
+    name = "Test Target",
+    metals = {
+      runType = "testTarget",
+    },
+  },
+}
+
+metals_config.on_attach = function(client, bufnr)
+  require("metals").setup_dap()
+end
+
 -- Autocmd that will actually be in charging of starting the whole thing
 local nvim_metals_group = api.nvim_create_augroup("nvim-metals", { clear = true })
 api.nvim_create_autocmd("FileType", {
