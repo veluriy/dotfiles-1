a9427469684f1bd30e7cadcf307d1ac69dfc73c9
wurzeit <hiccap.master.jedi@gmail.com>
2022-08-19 19:12:16 +0900

    [add] cousier path [fix] aa

--- .config/fish/conf.d/path.fish
+++ .config/fish/conf.d/path.fish
@@ -1,9 +1,10 @@
-#              _   _     
-#  _ __   __ _| |_| |__  
-# | '_ \ / _` | __| '_ \ 
+#              _   _
+#  _ __   __ _| |_| |__
+# | '_ \ / _` | __| '_ \
 # | |_) | (_| | |_| | | |
 # | .__/ \__,_|\__|_| |_|
 # |_|
 
 set PATH $HOME/.cargo/bin $PATH
 set PATH $HOME/.ghcup/bin $PATH
+set PATH $HOMR/.local/share/coursier/bin $PATH
