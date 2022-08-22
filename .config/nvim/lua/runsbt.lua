61c4f1be9776849d5db12531296ecf5fc625554d
wurzeit <hiccap.master.jedi@gmail.com>
2022-08-22 11:20:30 +0900

    fix runsbt

--- .config/nvim/lua/runsbt.lua
+++ .config/nvim/lua/runsbt.lua
@@ -1,9 +1,7 @@
-local function run_sbt()
+function run_sbt()
   vim.cmd("vsplit | terminal")
   local command = ':call jobsend(b:terminal_job_id, "sbt\\n")'
   vim.cmd(command)
 end
 
-return {
-  run_sbt = run_sbt
-}
+vim.api.nvim_set_keymap('n', 'sbt', '<Cmd>lua run_sbt()<CR>', { noremap = true, silent = true })
