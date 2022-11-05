32f421fcddeec7ef2c74079100a40184f7a86d48
wurzeit <hiccap.master.jedi@gmail.com>
2022-11-05 17:44:12 +0900

    [fix] nvim clear plugin

--- .config/nvim/lua/autocmds.lua
+++
@@ -1,9 +0,0 @@
-vim.api.nvim_exec(
-  [[
-  augroup fileTypeIndent
-    autocmd!
-    autocmd FileType *.ts,*.tsx,*.module.css setlocal tabstop=4 softtabstop=4 shifttabwidth=4
-  augroup END
-  ]],
-  true
-)
