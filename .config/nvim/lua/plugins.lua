6ffbbc187d80131c202d547f7ba79a83ec6f8c7e
wurzeit <hiccap.master.jedi@gmail.com>
2022-11-06 16:33:15 +0900

    [add] lualine settings

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -5,5 +5,51 @@ require('jetpack.packer').startup(function(use)
   use { 'tani/vim-jetpack', opt = 1 }-- bootstrap
   use { 'tjdevries/colorbuddy.nvim' }-- required neosolarized
   use { 'svrana/neosolarized.nvim' }-- color scheme
-  use { 'nvim-lualine/lualine.nvim' }-- status line
+  use { 'nvim-lualine/lualine.nvim', requires = { 'kyazdani42/nvim-web-devicons' } }-- status line
 end)
+
+---- lualine
+
+local lualineStatus, lualine = pcall(require, "lualine")
+if (not lualineStatus) then return end
+
+lualine.setup {
+  options = {
+    icons_enabled = true,
+    theme = 'solarized_dark',
+    section_separators = { left = '', right = '' },
+    component_separators = { left = '', right = '' },
+    disabled_filetypes = {}
+  },
+  sections = {
+    lualine_a = { 'mode' },
+    lualine_b = { 'branch' },
+    lualine_c = { {
+      'filename',
+      file_lualineStatus = true,
+      path = 0
+    } },
+    lualine_x = {
+      { 'diagnostics', sources = { "nvim_diagnostic" }, symbols = { error = ' ', warn = ' ', info = ' ',
+        hint = ' ' } },
+      'encoding',
+      'filetype'
+    },
+    lualine_y = { 'progress' },
+    lualine_z = { 'location' }
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = { {
+      'filename',
+      file_lualineStatus = true,
+      path = 1
+    } },
+    lualine_x = { 'location' },
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  extensions = { 'fugitive' }
+}
