dd73034741c28cf8aec3e49267b1626e4be417e2
wurzeit <hiccap.master.jedi@gmail.com>
2022-11-06 17:04:06 +0900

    [add] telescope

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -13,7 +13,11 @@ require('jetpack.packer').startup(function(use)
   use { 'lewis6991/gitsigns.nvim' }-- git
   use { 'f-person/git-blame.nvim' }-- git blame
   use { 'TimUntersberger/neogit' }-- work in progress vim
-  use { 'nvim-lua/plenary.nvim' }-- required neogit
+  use { 'nvim-lua/plenary.nvim' }-- required neogit, telescope
+  use { 'BurntSushi/ripgrep' }-- required telescope
+  use { 'sharkdp/fd' }-- required telescope
+  use { 'nvim-telescope/telescope.nvim' }-- high performance file searcher
+  use { 'nvim-telescope/telescope-file-browser.nvim' }-- telescope extension
 end)
 
 ---- lualine.nvim
@@ -173,3 +177,81 @@ vim.g.gitblame_message_templete = '<author>・<date>・<summary>'
 local neogit = require('neogit')
 neogit.setup {}
 vim.keymap.set('n', 'git', '<Cmd>Neogit<CR>')
+
+---- telescope
+
+local telescopeStatus, telescope = pcall(require, "telescope")
+if (not telescopeStatus) then return end
+local actions = require('telescope.actions')
+local builtin = require("telescope.builtin")
+
+local function telescope_buffer_dir()
+  return vim.fn.expand('%:p:h')
+end
+
+local fb_actions = require "telescope".extensions.file_browser.actions
+
+telescope.setup {
+  defaults = {
+    mappings = {
+      n = {
+        ["q"] = actions.close
+      },
+    },
+  },
+  extensions = {
+    file_browser = {
+      theme = "dropdown",
+      hijack_netrw = true,
+      mappings = {
+        ["i"] = {
+          ["<C-w>"] = function() vim.cmd('normal vbd') end,
+        },
+        ["n"] = {
+          ["N"] = fb_actions.create,
+          ["h"] = fb_actions.goto_parent_dir,
+          ["/"] = function()
+            vim.cmd('startinsert')
+          end
+        },
+      },
+    },
+  },
+}
+telescope.load_extension("file_browser")
+
+------ keymaps
+vim.keymap.set('n', ';f',
+  function()
+    builtin.find_files({
+      no_ignore = false,
+      hidden = true
+    })
+  end)
+vim.keymap.set('n', ';r', function()
+  builtin.live_grep()
+end)
+vim.keymap.set('n', '\\\\', function()
+  builtin.buffers()
+end)
+vim.keymap.set('n', ';t', function()
+  builtin.help_tags()
+end)
+vim.keymap.set('n', ';;', function()
+  builtin.resume()
+end)
+vim.keymap.set('n', ';e', function()
+  builtin.diagnostics()
+end)
+vim.keymap.set("n", "sf", function()
+  telescope.extensions.file_browser.file_browser({
+    path = "%:p:h",
+    cwd = telescope_buffer_dir(),
+    respect_gitignore = false,
+    hidden = true,
+    grouped = true,
+    previewer = false,
+    initial_mode = "normal",
+    layout_config = { height = 40 }
+  })
+end)
