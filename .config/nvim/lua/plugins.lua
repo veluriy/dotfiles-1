d2018022328c5243aea30012351fdf1ee2916043
wurzeit <hiccap.master.jedi@gmail.com>
2022-11-20 00:52:04 +0900

    [add] metals

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -389,6 +389,10 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
   vim.lsp.diagnostic.on_publish_diagnostics, { virtual_text = false }
 )
 
+-------- metals
+
+require'lspconfig'.metals.setup{}
+
 ------ cmp
 
 vim.opt.completeopt = "menu,menuone,noselect"
