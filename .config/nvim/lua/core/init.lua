5826027c9488e3c1845efe7e0987e78c3a06f566
wurzeit <hiccap.master.jedi@gmail.com>
2022-12-10 01:39:19 +0900

    [up] setting leader map

--- .config/nvim/lua/core/init.lua
+++ .config/nvim/lua/core/init.lua
@@ -1,4 +1,11 @@
+local leader_map = function ()
+    vim.g.mapleader = ' '
+    vim.api.nvim_set_keymap('n', ' ', '', { noremap = true })
+    vim.api.nvim_set_keymap('x', ' ', '', { noremap = true })
+end
+
 local load_core = function()
+    leader_map()
     require("core.plugins")
     require("core.options")
     require("keymap")
