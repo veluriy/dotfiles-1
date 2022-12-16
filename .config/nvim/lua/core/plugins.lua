67aebe9a389250cbbdfa3b2039f667275af9f490
wurzeit <hiccap.master.jedi@gmail.com>
2022-12-16 14:08:32 +0900

    notifyを削除

--- .config/nvim/lua/core/plugins.lua
+++ .config/nvim/lua/core/plugins.lua
@@ -553,15 +553,6 @@ return require('packer').startup({function(use)
         'folke/noice.nvim',
         requires = {
             { 'MunifTanjim/nui.nvim' },
-            {
-                'rcarriga/nvim-notify',
-                config = function ()
-                    require('notify').setup {
-                        background_colour = '#000000',
-                        stages = 'fade',
-                    }
-                end
-            },
         },
         config = function ()
             require('noice').setup {
