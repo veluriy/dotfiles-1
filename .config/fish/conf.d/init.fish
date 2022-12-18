f1628f30a57382331dd2a9b7e6667a775e6a7c26
wurzeit <hiccap.master.jedi@gmail.com>
2022-12-18 22:12:03 +0900

    [fix] 不要なaaを削除

--- .config/fish/conf.d/init.fish
+++ .config/fish/conf.d/init.fish
@@ -1,9 +1,2 @@
-#  _       _ _
-# (_)_ __ (_) |_
-# | | '_ \| | __|
-# | | | | | | |_
-# |_|_| |_|_|\__|
-
-# fortune
 set fish_greeting
 fish_vi_key_bindings
--- .config/fish/conf.d/path.fish
+++ .config/fish/conf.d/path.fish
@@ -1,10 +1,3 @@
-#              _   _
-#  _ __   __ _| |_| |__
-# | '_ \ / _` | __| '_ \
-# | |_) | (_| | |_| | | |
-# | .__/ \__,_|\__|_| |_|
-# |_|
-
 # homebrew
 fish_add_path /opt/homebrew/bin
 fish_add_path /home/linuxbrew/.linuxbrew/bin/brew
